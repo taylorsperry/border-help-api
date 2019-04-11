aa0240e978542a32e437877b8519bccde71fdc28
Taylor Sperry <sperry.taylor@gmail.com>
2019-04-10 21:33:58 -0600

    set up backend

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
+import { rights, scenarios } from './data';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// app.locals.rights = require(rights)
+// app.locals.scenarios = scenarios
+
 app.locals.rights = [
-  { id: 1, title: 'You have the right to remain silent', detail: 'Detail for this right'}
+  { id: 1, title: 'You have the right to remain silent', detail: 'Detail for this right' }
 ]
 
 app.locals.scenarios = [
-  { id: 'a', title: 'You are asked for documents', detail: 'Detail for this scenario'}
+    { id: 'a', title: 'You are asked for documents', detail: 'Detail for this scenario' }
 ]
 
 app.get('/api/v1/rights', (request, response) => {
   response.status(200).json(app.locals.rights)
 })
+
+app.get('api/v1/scenarios', (request, response) => {
+  response.status(200).json(app.locals.scenarios)
+})
+
 export default app
---
+++ data.js
@@ -0,0 +1,17 @@
+export const rights = [
+  { 
+    id: 1, 
+    title: 'You have the right to remain silent', 
+    detail: 'Detail for this right'
+  }
+]
+
+export const scenarios = [
+  { 
+    id: 'a', 
+    title: 'You are asked for documents', 
+    detail: 'Detail for this scenario'
+  }
+]
+
+export default data
