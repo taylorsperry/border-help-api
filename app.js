f978ad9a87e87ed67c3e4422c7e9770f4f221186
Taylor Sperry <sperry.taylor@gmail.com>
2019-04-14 14:32:02 -0600

    update data

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
-import express from 'express';
-import cors from 'cors';
-import { rights, scenarios } from './data';
+// import express from 'express';
+// import cors from 'cors';
+
+const express = require('express')
+const cors = require('cors')
+const { intro, rights, scenarios, help } = require('./data')
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.locals.rights = require(rights)
-// app.locals.scenarios = scenarios
+app.locals.intro = intro;
+app.locals.rights = rights;
+app.locals.scenarios = scenarios;
+app.locals.help = help;
 
-app.locals.rights = [
-  { id: 1, title: 'You have the right to remain silent', detail: 'Detail for this right' }
-]
-
-app.locals.scenarios = [
-    { id: 'a', title: 'You are asked for documents', detail: 'Detail for this scenario' }
-]
+app.get('/api/v1/intro', (request, response) => {
+  response.status(200).json(app.locals.intro)
+})
 
 app.get('/api/v1/rights', (request, response) => {
   response.status(200).json(app.locals.rights)
 })
 
-app.get('api/v1/scenarios', (request, response) => {
+app.get('/api/v1/scenarios', (request, response) => {
   response.status(200).json(app.locals.scenarios)
+})
+
+app.get('/api/v1/help', (request, response) => {
+  response.status(200).json(app.locals.help)
 })
 
 export default app
